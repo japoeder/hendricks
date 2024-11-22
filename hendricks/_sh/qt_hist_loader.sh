4eb9445ce1b7219a34b81c2dc29d10c68b69a31d
japoeder <japoeder@gmail.com>
2024-11-22 17:04:10 -0600

    change local ip for mongo.

--- hendricks/_sh/qt_hist_loader.sh
+++ hendricks/_sh/qt_hist_loader.sh
@@ -3,7 +3,7 @@
 #TODO: test history loader shell script
 
 # Default values
-URL="http://poederhome.myvnc.com:8001/load_ticker"
+URL="http://192.168.1.10:8001/load_ticker"
 FROM_DATE="2024-10-03T09:30:00-04:00"
 TO_DATE="2024-10-04T00:59:32-04:00"
 BATCH_SIZE=50000
