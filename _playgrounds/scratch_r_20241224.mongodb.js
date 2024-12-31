12e174eedf714a1f24bae4dcea5dc721cb529604
japoeder <japoeder@gmail.com>
2024-12-30 21:50:15 -0500

    fix last month bug in FinLoader.

--- _playgrounds/scratch_r_20241224.mongodb.js
+++ _playgrounds/scratch_r_20241224.mongodb.js
@@ -7,3 +7,24 @@ db.getCollectionNames().forEach(function(collName) {
         db[collName].drop();
     }
 })
+
+db.getCollectionNames().forEach(function(collName) {
+    if(collName.endsWith("_empCt")) {
+        print("Dropping: " + collName);
+        db[collName].drop();
+    }
+})
+
+db.getCollectionNames().forEach(function(collName) {
+    if(collName.endsWith("_execComp")) {
+        print("Dropping: " + collName);
+        db[collName].drop();
+    }
+})
+
+db.getCollectionNames().forEach(function(collName) {
+    if(collName.endsWith("_grade")) {
+        print("Dropping: " + collName);
+        db[collName].drop();
+    }
+})
