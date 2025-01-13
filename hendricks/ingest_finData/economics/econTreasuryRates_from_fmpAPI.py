62d12bdad1858d15fe12a506ec78225de0320afd
japoeder <japoeder@gmail.com>
2025-01-12 23:52:27 -0600

    fix treasury bug.

--- hendricks/ingest_finData/economics/econTreasuryRates_from_fmpAPI.py
+++ hendricks/ingest_finData/economics/econTreasuryRates_from_fmpAPI.py
@@ -89,6 +89,7 @@ def econTreasuryRates_from_fmpAPI(
             source="fmp",
             from_date=from_date,
             to_date=to_date,
+            ticker=ticker,
         )
 
         print(f"URL: {url}")
